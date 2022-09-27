6b84506252cacca7ed61c683648dcd62b27645b8
Rodrigo Gouveia <rsoares.developer@gmail.com>
2022-09-27 19:12:27 -0400

    Aula - 01

---
+++ module1/aula1.js
@@ -0,0 +1,11 @@
+//Javascript
+
+// - Estado do JS (https://2022.stateofjs.com/en-US/technologies/)
+// - Executado no tradutor, tradutor mais encontrado nos navegadores
+// - Omniresente na web (?)
+// - App híbridos (React-native, Angular...)
+// - plicação de ponta-a-ponta: banco de dados, back-end / front-end
+// - Interagir com o DOM - árvore de objetos do HTML (api-web)
+// - Requisições dinâmicas
+// - IoT (Internet das coisas): O JS está presente em tudo (?)
+// - ECMAScript (JavaScript)
