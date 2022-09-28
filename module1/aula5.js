85008f588a5ea2dc6f4d71215ba304e9996c426f
Rodrigo Gouveia <rsoares.developer@gmail.com>
2022-09-27 22:34:56 -0400

    Aula - 05

---
+++ module1/aula5.js
@@ -0,0 +1,45 @@
+//Operadores
+
+// Soma: +
+// subtração -
+// multiplicador *
+// divisor /
+// Resto de divisão %
+// Math: randon(), round(), sqrt() (que? oi?)
+
+// Atribuiçãp
+
+a = b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a++
+a--
+
+// Operadores de compração
+
+//  Atribuição
+a = b
+
+//  Comparação
+a == b
+
+// COmparação valor e tipo
+a === b
+
+// Diferença
+a != b
+
+// Maior
+a > b
+
+// Menor
+a < b
+
+// Maior igual
+a >= b
+
+// Menor igual
+a <= b
